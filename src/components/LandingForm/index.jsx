cfd3c4d910635a2f3676382f8a476c6f34dbacfb
leandro la greca <leandrolagreca@hotmail.com>
2022-11-03 14:01:36 -0300

    update

--- src/components/LandingForm/index.jsx
+++ src/components/LandingForm/index.jsx
@@ -126,7 +126,7 @@ export default function LandingForm({ register, setRegister }) {
   }
   async function handleReset(email) {
     const actionCodeSettings = {
-      url: "https://gamescript.vercel.app/home",
+      url: "https://gamescript-proyect.herokuapp.com/home",
       handleCodeInApp: true,
     };
     sendPasswordResetEmail(auth, (email = userInfo.email), actionCodeSettings);
