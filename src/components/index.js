c4e5edd9ba8086a63d40307a4fb0b2998a0c9ef4
leandro la greca <leandrolagreca@hotmail.com>
2023-01-07 16:07:34 -0300

    filters export delete

--- src/components/index.js
+++ src/components/index.js
@@ -9,7 +9,6 @@ export { default as AddToCartButton } from './AddToCartBtn';
 export { default as AddToWishes } from './AddToWishBtn';
 export { default as SessionButton } from './SessionButton';
 export { default as CartCard } from './CartCard';
-export { default as Filter } from './Filters/filtro';
 export { default as AddressForm } from './AddressForm/AddressForm';
 export { default as PaymentForm } from './PaymentForm/PaymentForm';
 export { default as Review } from './Review/Review';
