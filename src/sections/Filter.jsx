c13f3eb36977fe9a2e83e07f8ca224303458cadc
leandro la greca <leandrolagreca@hotmail.com>
2023-01-07 16:19:51 -0300

    import filter error

--- src/sections/Filter.jsx
+++ src/sections/Filter.jsx
@@ -1,5 +1,5 @@
 import { Box } from "@mui/material";
-import { Filter } from "../components";
+//import { Filter } from "../components";
 
 import DisableElevation from "../components/Filters/CleanButton";
 
