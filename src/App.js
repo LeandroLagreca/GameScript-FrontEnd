5c30dcedfcd5895085b637f9a8f3fa0a2a94067d
leandro la greca <leandrolagreca@hotmail.com>
2023-01-09 00:32:51 -0300

    app.js url

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import { BrowserRouter } from 'react-router-dom';
 import Routes from './Routes';
 import './App.css';
 import {ColorContextProvider} from './components/Theme/Theme';
+import axios from 'axios';
 
+axios.defaults.baseURL = 'https://gamescript-backend-production.up.railway.app/'
 
 
 function App() {
